4d74b03bd327bf9b61ac50667d02429c4edd6be2
ClaudioAGS <no-reply>
2019-09-09 02:00:22 -0300

    Ajustes pre aula 01 Claudio

---
+++ slide.bat
@@ -0,0 +1,5 @@
+rem C:\Users\Claudio\Anaconda3\Scripts\jupyter-nbconvert.exe 01_introducao.ipynb --to slides --post serve --SlidesExporter.reveal_theme=serif --SlidesExporter.reveal_scroll=True --SlidesExporter.reveal_transition=none
+
+C:\Users\Claudio\Anaconda3\Scripts\jupyter-nbconvert.exe %1 --to slides --post serve --SlidesExporter.reveal_theme=serif --SlidesExporter.reveal_scroll=True --SlidesExporter.reveal_transition=none
+
+
